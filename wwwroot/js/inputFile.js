00c7027e0b0fabd990e065515fa0d5f0cade7047
Ivan <vanya71310@gmail.com>
2020-03-22 11:58:33 +0300

    bag fix

---
+++ wwwroot/js/inputFile.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+{
+    let formInput = document.querySelector('.form-upload__input');
+  
+    formInput.addEventListener('change', function () {
+        console.log(1);
+        document.querySelector('.form-upload__filename').innerText = formInput.files[0].name;
+    });
+}
